bb1f52019875a67c2537c8bd5acd0635aeb20722
Josiah Yeow <josiahyeow.dev@gmail.com>
2022-05-31 22:56:13 +1000

    fix apollo client url

--- apollo-client.ts
+++ apollo-client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_VERCEL_URL + "/api/graphql",
+  uri: "/api/graphql",
   cache: new InMemoryCache(),
 });
 
